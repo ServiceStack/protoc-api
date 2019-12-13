8597d406d55a13af425f5b9f38886f1be8256b2a
Demis Bellot <demis.bellot@gmail.com>
2019-12-12 22:33:02 -0500

    Update ProtocConfig.cs

--- ProtocApi.ServiceInterface/ProtocConfig.cs
+++ ProtocApi.ServiceInterface/ProtocConfig.cs
@@ -160,7 +160,7 @@ namespace ProtocApi.ServiceInterface
                 {
                     Lang.Swift, new ProtocOptions("swift", "Swift") {
                         Args = new[] {
-//                            $"--plugin=protoc-gen-grpc={pluginPath("protoc-gen-swift")}",
+                            $"--plugin=protoc-gen-grpc={pluginPath("protoc-gen-swift")}",
                             "--grpc-swift_out=Client=true,Server=false:out",
                             "--swift_opt=Visibility=Public",
                         }
